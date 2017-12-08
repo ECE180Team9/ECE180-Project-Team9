fe24ac52238b7335d9a3deff0f6fffc19e5be49a
Novulux <jspham@ucsd.edu>
2017-12-07 16:10:07 -0800

    redid average salary by dev type

---
+++ AverageSalarybyType.py
@@ -0,0 +1,27 @@
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+questions = pd.read_csv('survey_results_schema.csv', header=0, sep=',')
+questions = questions['Column']
+chosen = questions.iloc[[0,1,3,5,11,14,18,28,43,45,46,53,81,87,88,89,90,96,129,130,142,143,152]].tolist()
+df= pd.read_csv('survey_results_public.csv', header=0, sep=',')[chosen]
+noNan = df.dropna(subset=['DeveloperType'])
+uniquedev =set()
+for index, row in df.iterrows():
+    if isinstance(row['DeveloperType'],basestring):
+        devs = row['DeveloperType'].split(";")
+        devs= map(str.strip, devs)
+        uniquedev.update(set(devs))
+        continue
+salaries = {}
+for devType in uniquedev:
+    dev = noNan.loc[noNan['DeveloperType'].str.contains(devType)]
+    salaries[devType] = dev['Salary'].mean() 
+    
+del salaries['Other']
+salaries = pd.DataFrame(salaries.items(), columns=["DeveloperType", 'Average Salary'])
+salaries = salaries.set_index('DeveloperType')
+salaries.plot(kind='barh')
+plt.show()
+
